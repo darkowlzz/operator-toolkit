337033a2165dd2ad54ce6fa3925510b49d996d30
Sunny <darkowlzz@protonmail.com>
2020-12-07 15:34:03 +0530

    update operator/v1/ godoc

--- operator/v1/doc.go
+++ operator/v1/doc.go
@@ -1,4 +1,4 @@
-// Package v1 contains helpers to build an operate component of a controller
-// reconciler. It provides patterns and tools to structure the operation
+// Package v1 contains helpers to build an operator component of a controller
+// reconciler. It provides patterns and tools to structure the operator
 // implementation.
 package v1
