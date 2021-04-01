e2bb0e1eca4739c568df0aaddf1fa05f6eec6cee
Sunny <darkowlzz@protonmail.com>
2021-04-01 17:51:03 +0530

    Add tracing support in stateless-action controller

--- controller/stateless-action/v1/reconcile.go
+++ controller/stateless-action/v1/reconcile.go
@@ -2,11 +2,13 @@ package v1
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/label"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -88,9 +90,11 @@ func (r *Reconciler) Init(mgr ctrl.Manager, ctrlr Controller, opts ...Reconciler
 }
 
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, reterr error) {
-	tr := otel.Tracer("Reconcile")
-	_, span := tr.Start(ctx, "reconcile")
+	tr := otel.Tracer(r.name)
+	ctx, span := tr.Start(ctx, r.name+": reconcile")
 	defer span.End()
+
+	span.SetAttributes(label.String("object-key", req.NamespacedName.String()))
 
 	controller := r.ctrlr
 
@@ -102,24 +106,29 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ct
 	// from other backend, return a nil object.
 	obj, err := controller.GetObject(ctx, req.NamespacedName)
 	if err != nil {
+		span.RecordError(err)
 		reterr = client.IgnoreNotFound(err)
 		return
 	}
 	// Return if the object is nil.
 	if obj == nil {
+		span.AddEvent("empty object")
 		return
 	}
 
 	// Check if an action is required.
 	requireAction, err := controller.RequireAction(ctx, obj)
 	if err != nil {
+		span.RecordError(err)
 		reterr = err
 		return
 	}
 
 	// If an action is required, run an action manager for the target object.
 	if requireAction {
+		span.AddEvent("Action required, running action manager")
 		if err := r.RunActionManager(ctx, obj); err != nil {
+			span.RecordError(err)
 			reterr = err
 			return
 		}
@@ -131,21 +140,31 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ct
 // RunActionManager runs the actions in the action manager based on the given
 // object.
 func (r *Reconciler) RunActionManager(ctx context.Context, o interface{}) error {
+	tr := otel.Tracer(r.name)
+	ctx, span := tr.Start(ctx, r.name+": run action manager")
+	defer span.End()
+
+	span.AddEvent("Build action manager")
 	actmgr, err := r.ctrlr.BuildActionManager(o)
 	if err != nil {
+		span.RecordError(err)
 		return errors.Wrapf(err, "failed to build action manager")
 	}
 
 	// Get the objects to run action on.
 	objects, err := actmgr.GetObjects(ctx)
 	if err != nil {
+		span.RecordError(err)
 		return errors.Wrapf(err, "failed to get objects from action manager")
 	}
+
+	span.AddEvent(fmt.Sprintf("Running actions for %d objects", len(objects)))
 
 	// Run the action in a goroutine.
 	for _, obj := range objects {
 		go func(o interface{}) {
 			if runErr := r.RunAction(actmgr, o); runErr != nil {
+				span.RecordError(runErr)
 				r.log.Info("failed to run action", "error", runErr)
 			}
 		}(obj)
@@ -157,30 +176,43 @@ func (r *Reconciler) RunActionManager(ctx context.Context, o interface{}) error 
 // RunAction checks if an action needs to be run before running it. It also
 // runs a deferred function at the end.
 func (r *Reconciler) RunAction(actmgr action.Manager, o interface{}) (retErr error) {
+	// Create a context with timeout to be able to cancel the action if it
+	// can't be completed within the given time.
+	ctx, cancel := context.WithTimeout(context.Background(), r.actionTimeout)
+	defer cancel()
+
+	tr := otel.Tracer(r.name + "-action")
+	ctx, span := tr.Start(ctx, r.name+": run action")
+	defer span.End()
+
 	name, err := actmgr.GetName(o)
 	if err != nil {
 		retErr = errors.Wrapf(err, "failed to get action manager name")
 		return
 	}
 
-	// Set up the logger with action info.
-	log := r.log.WithValues("action-manager", name)
+	span.SetAttributes(
+		label.String("actionName", name),
+		label.Int64("timeout", int64(r.actionTimeout)),
+		label.Int64("retryPeriod", int64(r.actionRetryPeriod)),
+	)
 
-	// Create a context with timeout to be able to cancel the action if it
-	// can't be completed within the given time.
-	ctx, cancel := context.WithTimeout(context.Background(), r.actionTimeout)
-	defer cancel()
+	// Set up the logger with action info.
+	log := r.log.WithValues("action", name)
 
 	// Defer the action Defer() function.
 	defer func() {
 		if deferErr := actmgr.Defer(ctx, o); deferErr != nil {
+			span.RecordError(deferErr)
 			retErr = errors.Wrapf(deferErr, "failed to run deferred action")
 			return
 		}
 	}()
 
 	// First run, handle any failure by continuing execution and retry.
+	span.AddEvent("First action run")
 	if runErr := actmgr.Run(ctx, o); runErr != nil {
+		span.RecordError(runErr)
 		log.Info("action run failed, will retry", "error", runErr)
 	}
 
@@ -190,19 +222,24 @@ func (r *Reconciler) RunAction(actmgr action.Manager, o interface{}) (retErr err
 		case <-time.After(r.actionRetryPeriod):
 			checkResult, checkErr := actmgr.Check(ctx, o)
 			if checkErr != nil {
+				span.RecordError(checkErr)
 				log.Info("failed to perform action check, retrying", "error", checkErr)
 				continue
 			}
 			if checkResult {
+				span.AddEvent("Check result true, rerun action")
 				if runErr := actmgr.Run(ctx, o); runErr != nil {
+					span.RecordError(runErr)
 					log.Info("action run retry failed", "error", runErr)
 				}
 			} else {
 				// Action successful, end the action.
+				span.AddEvent("action successful")
 				log.V(6).Info("action successful", "object", o)
 				return
 			}
 		case <-ctx.Done():
+			span.AddEvent("context cancelled")
 			log.Info("context cancelled, terminating action")
 			return
 		}
