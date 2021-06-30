9a818cc1e580e1075bfa730a4dd9743b67de1020
Sunny <darkowlzz@protonmail.com>
2021-06-30 23:31:56 +0530

    decl/kust: AddImages() handle duplicate img input
    Handle duplicate image entries in the image list passed to AddImages().

--- declarative/kustomize/mutate.go
+++ declarative/kustomize/mutate.go
@@ -67,12 +67,31 @@ func AddResources(resources []string) MutateFunc {
 // existing entry with the same name is updated with the new entry.
 func AddImages(images []apitypes.Image) MutateFunc {
 	return func(k *apitypes.Kustomization) {
+		// Remove duplicate entries from the list of images, giving preference
+		// to the last instance of the image in the list.
+		uniqueImages := []apitypes.Image{}
+		imageMap := map[string]apitypes.Image{}
+		// Record the sequence of the images for a deterministic result.
+		sequence := []string{}
+		for _, i := range images {
+			if _, exists := imageMap[i.Name]; !exists {
+				sequence = append(sequence, i.Name)
+			}
+			imageMap[i.Name] = i
+		}
+		// Populate the unique images in the recorded sequence.
+		for _, iName := range sequence {
+			uniqueImages = append(uniqueImages, imageMap[iName])
+		}
+
 		newImages := []apitypes.Image{}
 
-		// Remove duplicate entries from existing images.
+		// Compare the images in kustomization and the image list and add
+		// unique images in kustomization to the new images list. This will get
+		// rid of any duplicates in the kustomization images.
 		for _, eImage := range k.Images {
 			found := false
-			for _, nImage := range images {
+			for _, nImage := range uniqueImages {
 				// If entry for the same name is found, ignore it.
 				if eImage.Name == nImage.Name {
 					found = true
@@ -85,7 +104,7 @@ func AddImages(images []apitypes.Image) MutateFunc {
 		}
 
 		// Add new images.
-		newImages = append(newImages, images...)
+		newImages = append(newImages, uniqueImages...)
 
 		// Set kustomization images.
 		k.Images = newImages
--- declarative/kustomize/mutate_test.go
+++ declarative/kustomize/mutate_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/darkowlzz/operator-toolkit/declarative/loader"
 	"github.com/stretchr/testify/assert"
 	apitypes "sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/yaml"
 )
 
 func TestMutate(t *testing.T) {
@@ -91,4 +92,116 @@ resources:
 	result, err := fs.ReadFile(kustomizationFile)
 	assert.Nil(t, err)
 	assert.Equal(t, wantKustomize, string(result))
+}
+
+func TestAddImages(t *testing.T) {
+	existingKustomizeContent := `apiVersion: kustomize.config.k8s.io/v1beta1
+images:
+- name: someAppA
+  newName: example/AppA
+  newTag: v0.5.0
+- digest: sha256:25a0d4
+  name: someAppB
+  newName: example/AppB
+- name: someAppX
+  newName: foo/AppX
+  newTag: v5
+kind: Kustomization
+`
+
+	kustomization, err := LoadKustomization([]byte(existingKustomizeContent))
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name          string
+		images        []apitypes.Image
+		wantKustomize string
+	}{
+		{
+			name: "add new image",
+			images: []apitypes.Image{
+				{
+					Name: "fooAppA", NewName: "fooapp/appA", NewTag: "33",
+				},
+			},
+			wantKustomize: `apiVersion: kustomize.config.k8s.io/v1beta1
+images:
+- name: someAppA
+  newName: example/AppA
+  newTag: v0.5.0
+- digest: sha256:25a0d4
+  name: someAppB
+  newName: example/AppB
+- name: someAppX
+  newName: foo/AppX
+  newTag: v5
+- name: fooAppA
+  newName: fooapp/appA
+  newTag: "33"
+kind: Kustomization
+`,
+		},
+		{
+			name: "add existing image",
+			images: []apitypes.Image{
+				{
+					Name: "someAppA", NewName: "fooexample/AppY", NewTag: "v10",
+				},
+			},
+			wantKustomize: `apiVersion: kustomize.config.k8s.io/v1beta1
+images:
+- digest: sha256:25a0d4
+  name: someAppB
+  newName: example/AppB
+- name: someAppX
+  newName: foo/AppX
+  newTag: v5
+- name: fooAppA
+  newName: fooapp/appA
+  newTag: "33"
+- name: someAppA
+  newName: fooexample/AppY
+  newTag: v10
+kind: Kustomization
+`,
+		},
+		{
+			name: "add duplicate images",
+			images: []apitypes.Image{
+				{
+					Name: "someAppA", NewName: "fooexample/AppY", NewTag: "v10",
+				},
+				{
+					Name: "someAppA", NewName: "barexample/AppZ", NewTag: "v20",
+				},
+			},
+			wantKustomize: `apiVersion: kustomize.config.k8s.io/v1beta1
+images:
+- digest: sha256:25a0d4
+  name: someAppB
+  newName: example/AppB
+- name: someAppX
+  newName: foo/AppX
+  newTag: v5
+- name: fooAppA
+  newName: fooapp/appA
+  newTag: "33"
+- name: someAppA
+  newName: barexample/AppZ
+  newTag: v20
+kind: Kustomization
+`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			mutate := AddImages(tc.images)
+			mutate(kustomization)
+			r, err := yaml.Marshal(kustomization)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.wantKustomize, string(r))
+		})
+	}
 }
